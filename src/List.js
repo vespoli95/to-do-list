c9e1db76e84b5cd1d18a7d9038716b814a1f4109
vespoli95 <antvespoli@gmail.com>
2020-01-29 18:46:09 -0500

    update

--- src/List.js
+++ src/List.js
@@ -3,24 +3,38 @@ import React from 'react';
 let list_element = <span class="circle"></span>
 let items = [];
 
-function List(props){
-    for (let i = 0; i < 5; i++){
-        items.push(list_element);
+class List extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+        for (let i = 0; i < 5; i++){
+            items.push(list_element);
+        }
     }
-    return (
-        <div className="List">
-            <input type="button" id="add_task" value="Add Task" class="btn btn-success"/>
-            <ul id="to-do-list">
-                {               
-                    items.map((el, i) => {
-                        return <li class="list-member">{el}{props.name} {i + 1}</li>
-                    })
-                    
-                }
-            </ul>
-        </div>
 
-    );
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+    render() {
+        return (
+            <div className="List">
+                <input type="button" id="add_task" value="Add Task" class="btn btn-success"/>
+                <ul id="to-do-list">
+                    {               
+                        items.map((el, i) => {
+                            return <li class="list-member"><p>{this.state.isToggleOn ? 'yes' : 'no'}</p>{el}{this.props.name} {i + 1}</li>
+                        })
+                        
+                    }
+                </ul>
+            </div>
+    
+        );
+    }
+    
     
 }
 
