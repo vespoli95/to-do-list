d3d6b5a23c1c52f04523de4d44069cb2b520b91a
vespoli95 <antvespoli@gmail.com>
2020-01-29 21:40:03 -0500

    init

--- src/List.js
+++ src/List.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-let list_element = <span class="circle"></span>
-let items = [];
-
 class List extends React.Component {
     constructor(props){
         super(props);
-        this.state = {isToggleOn: true};
-        this.handleClick = this.handleClick.bind(this);
-        for (let i = 0; i < 5; i++){
-            items.push(list_element);
-        }
+        this.state = {
+            items: ["abc"],
+            isToggleOn: true
+        };
+        this.addItem = this.addItem.bind(this);
+        //this.handleClick = this.handleClick.bind(this);
+        let list_element = <li class="list-member" onclick={this.handleClick}><span class="circle"></span></li>
+      
     }
 
     handleClick() {
@@ -18,37 +18,47 @@ class List extends React.Component {
             isToggleOn: !state.isToggleOn
         }));
     }
+
+    addItem(e) {
+        this.setState((state) => {
+            var newState = this.state.items.push("anthony");
+            return {items: newState}
+        });
+    }
+
     render() {
         return (
             <div className="List">
-                <input type="button" id="add_task" value="Add Task" class="btn btn-success"/>
+                <form>
+                    <input type="text" placeholder="enter task" />
+                    <button type="button" id="add_task" class="btn btn-success" onClick={this.addItem()}>Add</button>
+                </form>
                 <ul id="to-do-list">
                     {               
-                        items.map((el, i) => {
-                            return <li class="list-member"><p>{this.state.isToggleOn ? 'yes' : 'no'}</p>{el}{this.props.name} {i + 1}</li>
+                        this.props.items.map((el, i) => {
+                            return <li class="list-member">{el} {i + 1}</li>
                         })
                         
                     }
                 </ul>
             </div>
-    
         );
     }
     
     
 }
 
-document.addEventListener('click', e =>
-{
-    console.log(e);
-    if (e.target.className.indexOf('circle') > -1){
-        if (e.target.className.indexOf('complete') === -1)
-            e.target.className += ' complete';
-        else 
-            e.target.className = 'circle';
-    } else if (e.target.id === "add_task"){
-        items.push(list_element);
-    }
-});
+// document.addEventListener('click', e =>
+// {
+//     console.log(e);
+//     if (e.target.className.indexOf('circle') > -1){
+//         if (e.target.className.indexOf('complete') === -1)
+//             e.target.className += ' complete';
+//         else 
+//             e.target.className = 'circle';
+//     } else if (e.target.id === "add_task"){
+//         items.push(list_element);
+//     }
+// });
 
 export default List;
